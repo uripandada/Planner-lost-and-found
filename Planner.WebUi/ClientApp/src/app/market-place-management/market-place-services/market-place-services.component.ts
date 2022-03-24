f44d58df07071ea6624c2761983e58afbbc7aefe
jhonpastry123 <jhon.pastry123@gmail.com>
2022-03-24 20:16:24 +1000

    change market place Ui

--- Planner.WebUi/ClientApp/src/app/market-place-management/market-place-services/market-place-services.component.ts
+++ Planner.WebUi/ClientApp/src/app/market-place-management/market-place-services/market-place-services.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
+import {ThemePalette} from '@angular/material/core';
 
 @Component({
   selector: 'app-market-place-services',
@@ -6,14 +7,21 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./market-place-services.component.css']
 })
 export class MarketPlaceServicesComponent implements OnInit {
-  
+  color: ThemePalette;
+  @Input() id: '';
   @Input() image: '';
   @Input() checkStatus: any;
   @Input() isChecked: any;
+  @Output() changeEvent = new EventEmitter();
   constructor() {}
 
   ngOnInit(): void {
-    this.isChecked = false;
+    this.color = "accent"
+  }
+
+  onChangeEvent(data: any) {
+    this.isChecked = data.checked;
+    this.changeEvent.emit(data);    
   }
 
 }
